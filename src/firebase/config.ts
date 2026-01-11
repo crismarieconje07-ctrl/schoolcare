a027746765cb29bcc57a1e45f37de24d068fdaa2
Crismarie Conje <crismarie.conje07@gmail.com>
2026-01-11 13:07:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6574634930-513da",
+  "appId": "1:514805444088:web:72445471f4df1a233b7a0d",
+  "apiKey": "AIzaSyCPy7waw3CgJobAYkkgjww02RyaR61u4KQ",
+  "authDomain": "studio-6574634930-513da.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "514805444088"
+};
