85f0687b92f4656369fd4bc18f989c1af3838587
Crismarie Conje <crismarie.conje07@gmail.com>
2026-01-13 02:47:51 +0000

    fix: remove duplicate firebase initializations

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,8 +1,3 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./config";
+import { auth } from "./config";
 
-const app =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-
-export const auth = getAuth(app);
+export { auth };
